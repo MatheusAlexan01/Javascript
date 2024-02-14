afd89daf7f33545f049ab0a3de59e25567c5bf83
MatheusRN <156456046+MatheusAlexan01@users.noreply.github.com>
2024-02-14 20:23:37 -0300

    Delete UC4/Atividades Primaria directory

--- UC4/Atividades Primaria/A2.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var litrosCd = 4;
-var pagamento = 35;
-var total = pagamento / litrosCd;
-console.log(`Deu um total de ${total} litros.`);
--- UC4/Atividades Primaria/A3.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var pesocd = 12;
-var KGPego = 6;
-var valorPago = pesocd * KGPego;
-console.log(`Ele vai precisar Paga ${valorPago}`);
