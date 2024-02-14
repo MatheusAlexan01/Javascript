9aafae10ddf68b4b299dbb3cac5f0772671f6f89
MatheusAlexan01 <matheus123.ma55@gmail.com>
2024-02-14 20:25:01 -0300

    Atividas Primarias

--- UC4/Atividades Primaria/A4.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let dia = 18;
-let mes = 12;
-let tempopassados = (mes * 30) - (30 - dia);
-console.log(` O dias passados foram ${tempopassados}`);
--- UC4/Atividades Primaria/helloword.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let pao = 0.12;
-let broa = 1.50;
-let qtdPaes = 5;
-let qtdBroa = 10;
-let arrecadacaoDia = (pao * qtdPaes) + (broa * qtdBroa);
-let porcentagem = arrecadacaoDia /100 * 10;
-console.log(arrecadacaoDia);
-console.log(porcentagem);
